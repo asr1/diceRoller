8eb6c43c072e847aad7623b3592511babf5db8a5
Alex Rinehart <alexandersrinehart@gmail.com>
2018-06-10 11:45:12 -0700

    Clean up count, add theoretical that works for 2 dice

---
+++ RollCount.js
@@ -0,0 +1,17 @@
+module.exports = class RollCount {
+    constructor(max)
+    {
+        this.count = [];
+        
+        //Prefill with 0s to prevent a bug where it can't differentiate between the number n and the nth element, which may move.
+        for(var i = 0; i < max; i++)
+        {
+            this.count.push({"sum": i+1, "amount": 0})
+        }
+    }
+
+    add(roll)
+    {
+        this.count[roll-1].amount++; //arrays start at 0, sums start at 1
+    }
+}
